08c6792f7a22b30d148186cb0c20a5ec6c434ced
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-12-09 17:36:45 +0100

    added editor toggles

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,40 +1,46 @@
 import styled from 'styled-components'
 
-interface NavbarProps {
+const Navbar: React.FC<{
   isPayloadVisible: boolean
   setIsPayloadVisible: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-const Navbar: React.FC<NavbarProps> = ({
+  isFieldsVisible: boolean
+  setIsFieldsVisible: React.Dispatch<React.SetStateAction<boolean>>
+}> = ({
   isPayloadVisible,
   setIsPayloadVisible,
+  isFieldsVisible,
+  setIsFieldsVisible,
 }) => {
   return (
-    <StyledNav>
+    <Nav>
       <MainHeading>JSON Permutator</MainHeading>
-      <StyledButton onClick={() => setIsPayloadVisible(!isPayloadVisible)}>
+      <Button onClick={() => setIsPayloadVisible(!isPayloadVisible)}>
         {isPayloadVisible ? 'Hide Payload' : 'Show Payload'}
-      </StyledButton>
-    </StyledNav>
+      </Button>
+      <Button onClick={() => setIsFieldsVisible(!isFieldsVisible)}>
+        {isFieldsVisible ? 'Hide Fields' : 'Show Fields'}
+      </Button>
+    </Nav>
   )
 }
 
-const MainHeading = styled.h1``
-
-const StyledNav = styled.nav`
+const Nav = styled.nav`
   display: flex;
   align-items: center;
-
-  & > *:not(:last-child) {
-    margin-right: 2rem;
-  }
-
   background: var(--button-background);
   padding: 0.5rem;
   margin-bottom: 1rem;
+
+  & > *:not(:last-child) {
+    margin-right: 1rem;
+  }
 `
 
-const StyledButton = styled.button`
+const MainHeading = styled.h1`
+  padding: 0 0.5rem;
+`
+
+const Button = styled.button`
   background-color: var(--background-color);
   color: var(--button-text-color);
   border: none;
