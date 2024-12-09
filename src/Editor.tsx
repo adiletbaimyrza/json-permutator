5c85053e165e738237865e1e2ef9849d0164a171
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-12-09 17:29:43 +0100

    editor finished

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MonacoEditor from '@monaco-editor/react'
-import Heading from './Heading'
-import { HeadingSize } from './types'
 import styled from 'styled-components'
 
 const Editor: React.FC<{
@@ -9,17 +7,28 @@ const Editor: React.FC<{
   value: string
   onChange: (newValue: string | undefined) => void
 }> = ({ heading, value, onChange }) => {
+  const [isFullScreen, setIsFullScreen] = useState<boolean>(false)
+
   const handleEditorChange = (value: string | undefined) => {
     onChange(value)
   }
 
+  const toggleFullScreen = () => {
+    setIsFullScreen(!isFullScreen)
+  }
+
   return (
-    <EditorWrapper>
-      <Heading size={HeadingSize.XS}>{heading}</Heading>
+    <EditorWrapper isFullScreen={isFullScreen}>
+      <EditorHeader>
+        <EditorHeading>{heading}</EditorHeading>
+        <EditorButton onClick={toggleFullScreen}>
+          {isFullScreen ? 'Exit Fullscreen' : 'Fullscreen'}
+        </EditorButton>
+      </EditorHeader>
 
       <MonacoEditor
-        height={400}
-        width={1000}
+        height={isFullScreen ? window.innerHeight - 20 : 400}
+        width={isFullScreen ? window.innerWidth - 20 : 800}
         language="json"
         value={value}
         options={{
@@ -32,8 +41,46 @@ const Editor: React.FC<{
   )
 }
 
-const EditorWrapper = styled.div`
+const EditorWrapper = styled.div<{ isFullScreen: boolean }>`
   margin-bottom: 1rem;
+  ${({ isFullScreen }) =>
+    isFullScreen &&
+    `
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #121212;
+    z-index: 9999;
+    padding: 1rem;
+  `}
+`
+
+const EditorButton = styled.button`
+  background-color: var(--button-background);
+  color: var(--button-text-color);
+  border: none;
+  border-radius: 0.25rem;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  font-size: 1rem;
+
+  &:hover {
+    background-color: var(--button-hover-background);
+  }
+`
+
+const EditorHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 0.5rem;
+`
+
+const EditorHeading = styled.h3`
+  margin: 0;
+  color: var(--text-color);
 `
 
 export default Editor
